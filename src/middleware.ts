9aa0fa2f86cd82e78de62477b42dde8cbe0abdd2
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-10-18 16:53:44 -0300

    fix: errors undefined

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { authMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk']);
-
-export default clerkMiddleware((auth, request) => {
-	if (!isPublicRoute(request)) {
-		auth().protect();
-	}
-});
+export default authMiddleware({
+	publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+})
 
 export const config = {
 	matcher: [
 		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
 		'/(api|trpc)(.*)',
 	],
-};
+}
