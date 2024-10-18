dd7e57cd74e5290464b219987cd080f81ce614aa
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-10-17 23:09:59 -0300

    feat: finalize webhook implementation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk']);
 
 export default clerkMiddleware((auth, request) => {
 	if (!isPublicRoute(request)) {
-		auth().protect()
+		auth().protect();
 	}
-})
+});
 
 export const config = {
 	matcher: [
 		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
 		'/(api|trpc)(.*)',
 	],
-}
+};
